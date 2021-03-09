67112002f94f1efa8b2886783b5c8069d9c4c764
Nyapal <nyapal14@gmail.com>
2021-03-08 22:14:21 -0800

    v 1.1 fr this time

--- makesite.go
+++ makesite.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"io/ioutil"
 	"html/template"
+	"path/filepath"
 )
 
 type firstpost struct {
@@ -21,24 +22,36 @@ func main() {
 	// 	fmt.Print("Houston, we have a prolem")
 	// }
 	// fmt.Print(string(file))
-
+	
+	fpost := " "
 	// Edit template.. 
-	// tmpl := template.Must(template.ParseFiles("first-post.txt"))
-	// tmpl.Execute(os.Stdout, fpost)
+	tmpl := template.Must(template.ParseFiles("first-post.txt"))
+	tmpl.Execute(os.Stdout, fpost)
 
-	// const html = "<!doctype html><head><title>Untitled Custom SSG</title></head><body></body></html>"
+	const html = "<!doctype html><head><title>Untitled Custom SSG</title></head><body></body></html>"
 
 	// Write HTML template to file 
 	t := template.Must(template.New("first-post.html").Parse(html))
 	fmt.Print(t)
 
-	// File Flag... 
+	// File Flag
 	file := flag.String(".txt", ".html", "Update file")
 	flag.Parse()
 	fmt.Println(*file)
 
 	// Dir Flag 
-	dir := flag.String()
-	
 	// find all the .txt files in the directory & print to standout
+	path := "~/go/makesite"
+	files, err := ioutil.ReadDir(path)
+	
+	for _, f := range files {
+		if filepath.Ext(f.Name()) == ".txt" {
+			fmt.Println(f.Name())
+		}
+	}
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
 }
